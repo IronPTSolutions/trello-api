1184420811e955c45c28571bb58e3f6e1d1e5ad0
Julio García Pérez <juliojose.garcia@11paths.com>
2019-03-06 17:20:03 +0100

    remove solution ;)

--- routes/cards.routes.js
+++ routes/cards.routes.js
@@ -1,12 +1,6 @@
 const express = require('express');
 const router = express.Router();
 
-const cardsController = require('../controllers/cards.controller');
-
-router.get('/', cardsController.list);
-router.post('/', cardsController.create);
-router.get('/:id', cardsController.get);
-router.put('/:id', cardsController.update);
-router.delete('/:id', cardsController.delete);
+// write your routes here
 
 module.exports = router;
--- routes/columns.routes.js
+++ routes/columns.routes.js
@@ -1,12 +1,6 @@
 const express = require('express');
 const router = express.Router();
 
-const columnsController = require('../controllers/columns.controller');
-
-router.get('/', columnsController.list);
-router.post('/', columnsController.create);
-router.get('/:id', columnsController.get);
-router.put('/:id', columnsController.update);
-router.delete('/:id', columnsController.delete);
+// write your routes here
 
 module.exports = router;
